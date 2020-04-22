43ad001f73106281d594a4ed3143e2764ae7c8dc
Skyler Dong <dongskyler@gmail.com>
2020-04-22 17:11:19 -0500

    configuring

---
+++ src/login/API_KEY_GOOG_route-optimization_public.php
@@ -0,0 +1,3 @@
+<?php
+define('API_KEY_GOOGLE','AIzaSyCpXLEfMiVNZq5JEW6J1GlIFsLnh412Ekg');
+?>
---
+++ src/login/login_mysql_bluehost.php
@@ -0,0 +1,6 @@
+<?php // login_mysql.php
+  $hn = 'localhost';
+  $db = 'theyove2_photo_collection';
+  $un = 'theyove2_gow7ach';
+  $pw = 'fr$swosP7frecef&qlw&';
+?>
