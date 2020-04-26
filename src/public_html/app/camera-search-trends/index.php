e22aa5ca8864a9b4f8a487a84104128a778b3cee
Skyler Dong <dongskyler@gmail.com>
2020-04-25 21:51:33 -0500

    Added revenue plot

--- src/public_html/app/camera-search-trends/index.php
+++ src/public_html/app/camera-search-trends/index.php
@@ -26,7 +26,7 @@
   <h1>Camera Brand Search Trends of Canon, Nikon and Sony</h1>
     <div class="row">
       <div class="col-12 mb-3 col-md-10">
-        <p><a href="https://github.com/tangaw" target="_blank">Aaron Tang</a> and I wrote <a href="https://skylerdong.com/blog/scrape-google-trends.php" target="_blank">a piece of code in Python to scrape Google trends data</a>. Here we present the data.</p>
+        <p><a href="https://github.com/tangaw" target="_blank">Aaron Tang</a> and I wrote <a href="https://skylerdong.com/coding/scrape-google-trends.php" target="_blank">a piece of code in Python to scrape Google trends data</a>. Here we present the data.</p>
         <p>We are interested in the search trends of 3 major camera brands (Canon, Nikon and Sony).</p>
         <!-- <p>We further subdivide cameras into 3 keywords: camera (in general), DSLR and mirrorless.<p> -->
         <p>Hence, we scraped the Google search trend data of 9 sets of keywords from January 2006 to March 2020 at a monthly resolution:</p>
@@ -54,6 +54,10 @@
           <h3>Google search trends of "mirrorless" by brand</h3>
         </div>
         <div id="plot-mirrorless"></div>
+        <div class="mb-3 text-center">
+          <h3>Annual revenues of camera divisions by brand</h3>
+        </div>
+        <div id="plot-revenue"></div>
         <p class="footnote">This is work in progress...</p>
         <!-- <div id="plot-map"></div> -->
       </div>
