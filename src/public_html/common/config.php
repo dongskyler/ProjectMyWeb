1caa96ada1d8a0037ec108727b24a8181508e438
Skyler Dong <dongskyler@gmail.com>
2020-04-28 02:48:26 -0500

    Updated config.php

--- src/public_html/common/config.php
+++ src/public_html/common/config.php
@@ -2,11 +2,11 @@
 <?php
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
   if (strpos(dirname(__FILE__), 'build/release')) {
-    define('PATH', 'http://localhost/ProjectMyWeb/build/release/public_html');
-  } elseif (strpos(dirname(__FILE__), 'ProjectMyWeb/src')) {
-    define('PATH', 'http://localhost/ProjectMyWeb/src/public_html');
-  } elseif (strpos(dirname(__FILE__), 'ProjectNextGen/src')) {
-    define('PATH', 'http://localhost/App/ProjectNextGen/src');
+    define('PATH', 'http://localhost/my-web/build/release/public_html');
+  } elseif (strpos(dirname(__FILE__), 'my-web/src')) {
+    define('PATH', 'http://localhost/my-web/src/public_html');
+  } elseif (strpos(dirname(__FILE__), 'camera-trends/src')) {
+    define('PATH', 'http://localhost/camera-trends/src');
   }
 } else
     define("PATH", 'https://skylerdong.com');
