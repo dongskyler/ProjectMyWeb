faeb73a8ad8ac864f874eea7b467914bbde5f5f2
Skyler Dong <dongskyler@gmail.com>
2020-05-04 05:59:44 -0500

    fixed the bug by updating maincontent.scss

--- src/public_html/common/config.php
+++ src/public_html/common/config.php
@@ -3,12 +3,9 @@
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
   if (strpos(dirname(__FILE__), 'build/release')) {
     define('PATH', 'http://localhost/ProjectMyWeb/build/release/public_html');
-  } elseif (strpos(dirname(__FILE__), 'ProjectMyWeb/src')) {
-    define('PATH', 'http://localhost/ProjectMyWeb/src/public_html');
-  } elseif (strpos(dirname(__FILE__), 'ProjectNextGen/src')) {
-    define('PATH', 'http://localhost/App/ProjectNextGen/src');
+  } elseif (strpos(dirname(__FILE__), 'my-web')) {
+    define('PATH', 'http://localhost/my-web/src/public_html');
   }
 } else
     define("PATH", 'https://skylerdong.com');
-    // define("PATH", $_SERVER['HTTP_HOST']);
 ?>
