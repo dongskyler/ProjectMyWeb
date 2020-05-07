5a628ad474b2f068075b3c3c00eef33c76fed15e
Skyler Dong <dongskyler@gmail.com>
2020-05-07 09:26:16 -0500

    Maintenance/updagte config php localhost path (#21)
    * Updated config.php
    
    * Updated config.php with new localhost path

--- src/public_html/common/config.php
+++ src/public_html/common/config.php
@@ -1,9 +1,7 @@
 <!-- Config -->
 <?php
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
-  if (strpos(dirname(__FILE__), 'build/release')) {
-    define('PATH', 'http://localhost/ProjectMyWeb/build/release/public_html');
-  } elseif (strpos(dirname(__FILE__), 'my-web')) {
+  if (strpos(dirname(__FILE__), 'my-web/src')) {
     define('PATH', 'http://localhost/my-web/src/public_html');
   }
 } else
