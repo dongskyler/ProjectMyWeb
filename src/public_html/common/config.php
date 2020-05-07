91893e62ef6891518790008b74a5786b539dc803
Skyler Dong <dongskyler@gmail.com>
2020-05-07 09:24:41 -0500

    Updated config.php with new localhost path

--- src/public_html/common/config.php
+++ src/public_html/common/config.php
@@ -1,12 +1,8 @@
 <!-- Config -->
 <?php
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
-  if (strpos(dirname(__FILE__), 'build/release')) {
-    define('PATH', 'http://localhost/my-web/build/release/public_html');
-  } elseif (strpos(dirname(__FILE__), 'my-web/src')) {
+  if (strpos(dirname(__FILE__), 'my-web/src')) {
     define('PATH', 'http://localhost/my-web/src/public_html');
-  } elseif (strpos(dirname(__FILE__), 'camera-trends/src')) {
-    define('PATH', 'http://localhost/camera-trends/src');
   }
 } else
     define("PATH", 'https://skylerdong.com');
