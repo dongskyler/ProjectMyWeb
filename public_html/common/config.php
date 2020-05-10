1339eb9e356f554f976529582a7590f63402c808
Skyler Dong <dongskyler@gmail.com>
2020-05-10 18:12:22 -0500

    fixed config.php and added mysql login, which is ignored by gitignored (#25)

--- public_html/common/config.php
+++ public_html/common/config.php
@@ -2,8 +2,8 @@
 <?php
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
   if (strpos(dirname(__FILE__), 'my-web-gen2')) {
-    define('PATH', 'http://localhost/my-web-gen2/html');
+    define('PATH', 'http://localhost/my-web-gen2/public_html');
   }
 } else
-    define("PATH", '.');
+    define("PATH", 'https://skylerdong.com');
 ?>
