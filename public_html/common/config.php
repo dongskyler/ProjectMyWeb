e8217931a18685422a2396b96144aca865976c6f
Skyler Dong <dongskyler@gmail.com>
2020-05-15 17:34:56 -0500

    Debug/fix path (#27)
    * Update config.php by defining PATH as variable for non-localhost
    
    * Revert "Update config.php by defining PATH as variable for non-localhost"
    
    This reverts commit a317cf8e7810dc239893ca1b90a56b995d686dfb.
    
    * Fixed path

--- public_html/common/config.php
+++ public_html/common/config.php
@@ -1,9 +1,9 @@
 <!-- Config -->
 <?php
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
-  if (strpos(dirname(__FILE__), 'my-web-gen2')) {
-    define('PATH', 'http://localhost/my-web-gen2/public_html');
+  if (strpos(dirname(__FILE__), 'skylerdong.com-gen2')) {
+    define('PATH', 'http://localhost/skylerdong.com-gen2/public_html');
   }
 } else
-    define("PATH", $_SERVER['HTTP_HOST']);
+    define("PATH", $_SERVER['SERVER_NAME']);
 ?>
