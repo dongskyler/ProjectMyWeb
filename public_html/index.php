3b181bf1ffa3cdf2c965c9e901b766926c8b7d4d
Skyler Dong <dongskyler@gmail.com>
2020-07-13 11:39:22 -0500

    Update homepage

--- public_html/index.php
+++ public_html/index.php
@@ -30,13 +30,19 @@ include_once 'common/htmlTag.php';
       <ul>
         <li>You're looking at the 2nd-generation of my personal website, built with PHP, JavaScript, and jQuery. Check out the <a href="https://github.com/dongskyler/gen2.skylerdong.com" target="_blank" rel="noferrer noopener">source code on GitHub</a>.</li>
         <li>
-          Currently, I'm building a <a href="https://gen3.skylerdong.com" target="_blank" rel="noferrer noopener">new 3rd-generation website</a> with a React front-end and a Node/Express back-end, with a Continuous Integration approach and a plug-in architecture:
+          Currently, I'm building a <a href="https://gen3.skylerdong.com/" target="_blank" rel="noferrer noopener">new 3rd-generation website</a> with a React front-end, a Node/Express back-end and cloud-based MongoDB (on AWS), with a Continuous Integration approach and a <a href="https://github.com/dongskyler/api.skylerdong.com#Architecture" target="_blank" rel=”noreferrer”>plug-in architecture</a>:
           <ul>
             <li><a href="https://gen3.skylerdong.com/" target="_blank" rel=”noreferrer”>New website.</a>
             </li>
             <li><a href="https://api.skylerdong.com/" target="_blank" rel=”noreferrer”>New API back-end.</a>
             </li>
           </ul>
+        </li>
+        <li>
+          Download my resume <a href="https://api.skylerdong.com/resume/" target="_blank" rel="noferrer noopener">here</a>.
+        </li>
+        <li>
+          I also co-founded <a href="https://badwaterbay.com/" target="_blank" rel=”noreferrer”>BadwaterBay</a>, a group of people who code open-source projects. <a href="https://github.com/BadwaterBay" target="_blank" rel=”noreferrer”>Check us out on GitHub</a>!
         </li>
       </ul>
     </div>
