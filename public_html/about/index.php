9da0ed9ffdec040d8a130feda10eb33f289b54e5
Skyler Dong <dongskyler@gmail.com>
2020-07-13 11:26:36 -0500

    Update About page

--- public_html/about/index.php
+++ public_html/about/index.php
@@ -4,18 +4,20 @@ define("MASTHEAD_SIZE", 'SM');
 define("BG_IMG_MD", 'photography/SD__A6T0512_Edit_Web.jpg');
 define("BG_IMG_LG", 'large/SD__A6T0512_Edit_Web_Large.jpg');
 define("SITE_TITLE", 'About');
-define("SITE_SUBTITLE", '<p>I&#39;m an aspiring&nbsp;<a href="'.PATH.'/app">web developer</a> and&nbsp;an&nbsp;<a href="'.PATH.'/photography/">amateur photographer</a></p><p>Read <a href="'.PATH.'/blog">my blogs here</a> and <a href="'.PATH.'/blog/this-website.php">how I&apos;building this website</a></p>');
+define("SITE_SUBTITLE", '<p>I&#39;m an aspiring&nbsp;<a href="' . PATH . '/app">web developer</a> and&nbsp;an&nbsp;<a href="' . PATH . '/photography/">amateur photographer</a></p><p>Read <a href="' . PATH . '/blog">my blogs here</a> and <a href="' . PATH . '/blog/this-website.php">how I&apos;building this website</a></p>');
 define("FADE_IN", 'no');
-define("OG_IMAGE", PATH.'/images/other/SD-IMG_4854-Edit-Web.jpg');
+define("OG_IMAGE", PATH . '/images/other/SD-IMG_4854-Edit-Web.jpg');
 ?>
 <!doctype html>
 <?php
 include_once '../common/htmlTag.php';
 ?>
+
 <head>
   <?php include_once '../common/head.php'; ?>
   <title>About - Skyler Dong</title>
 </head>
+
 <body>
   <?php
   include_once '../common/navbar.php';
@@ -30,7 +32,7 @@ include_once '../common/htmlTag.php';
         </div>
         <div>
           <p>I'm Tiannong "Skyler" Dong.</p>
-          <p>I'm an aspiring web developer.</p>
+          <p>I'm an aspiring web developer. Check out my <a href="https://github.com/dongskyler" target="_blank" rel="noreferrer noopener">GitHub</a>.</p>
         </div>
         <div class="col-12 float-none mb-3 photo-block text-center small col-sm-6 float-sm-right col-md-4">
           <img class="img-fluid" src="../images/other/SD-IMG_4854-Edit-Web.jpg" alt="Photo of Skyler Dong. Photo courtesy of Alexey Portnov.">Photo courtesy of Alexey Portnov
@@ -44,26 +46,30 @@ include_once '../common/htmlTag.php';
             <li>Read about <a href="<?php echo PATH; ?>/blog/this-website.php">how I'm building this website</a> with HTML, CSS, JavaScript, PHP and MySQL.</li>
           </ul>
         </div>
-        <div>I have a degree in science and I like making things and helping people.
+        <div>I have a degree in science but I also like making things and helping people.
           <ul>
             <li>I have built 2 websites in the past:
               <ul>
-                <li><a href="https://sites.google.com/site/tiannongsdong/" target="_blank" rel=”noreferrer”>My old personal website</a>, which has been online since I was an undergrad student.
+                <li><a href="https://gen1.skylerdong.com/" target="_blank" rel=”noreferrer”>My old personal website</a>, which has been online since I was an undergrad student.
                 </li>
-                <li><a href="https://sites.google.com/site/dongskyler/nav-ohare/shuttle-center" target="_blank" rel=”noreferrer”>How to nagivate to the shuttle center</a> at <a href="https://www.flychicago.com/ohare/home/pages/default.aspx" target="_blank" rel=”noreferrer”>O'Hare Airport of Chicago</a>. I made the website for new incoming students to my alma mater (Beloit College) and the general public. Many students fly to O'Hare and take an inter-state shuttle to the college. Reportedly, some people have trouble navigating to the shuttle center from terminals.</li>
-                <li>In the past, I used templates and modified their HTML codes, but now I'm building this website from scratch.</li>
+                <li><a href="https://sites.google.com/site/dongskyler/nav-ohare/shuttle-center" target="_blank" rel=”noreferrer”>How to nagivate to the shuttle center</a> at <a href="https://www.flychicago.com/ohare/home/pages/default.aspx" target="_blank" rel=”noreferrer”>O'Hare Airport of Chicago</a>. I made the website for new incoming students to my alma mater (Beloit College) and the general public because many students fly to O'Hare and take an inter-state shuttle to the college. Reportedly, some people have trouble navigating to the shuttle center from terminals.</li>
+                <li>In the past, I used templates, but now I'm building this website from scratch.</li>
               </ul>
             </li>
-            <li>I have built many electronic devices. To name two of them:
+            <li>You're looking at the 2nd-generation of my personal website, built with PHP, JavaScript, and jQuery.</li>
+            <li>
+              Currently, I'm building a new personal website with a React front-end and a Node/Express back-end, with a Continuous Integration approach and a plug-in architecture:
               <ul>
-                <li><a href="https://en.wikipedia.org/wiki/Coulombmeter" target="_blank" rel=”noreferrer”>Coulombmeters</a> for <a href="https://en.wikipedia.org/wiki/Coulometry" target="_blank" rel=”noreferrer”>coulometric titration (a chemistry technique)</a> using <a href="https://www.raspberrypi.org/help/what-%20is-a-raspberry-pi/" target="_blank" rel=”noreferrer”>Raspberry Pi's</a> with <a href="https://chemistry.beloit.edu/classes/lisensky/" target="_blank" rel=”noreferrer”>my chemistry professor George Lisensky</a>.</li>
-                <li>Homemade data acquisition system and <a href="https://en.wikipedia.org/wiki/Thermoelectric_cooling" target="_blank" rel=”noreferrer”>Peltier plate</a> cooling system using an <a href="https://www.arduino.cc/en/guide/introduction" target="_blank" rel=”noreferrer”>Arduino</a> for my graduate school research. The Arduino code was written in C.</li>
+                <li><a href="https://gen3.skylerdong.com/" target="_blank" rel=”noreferrer”>New website.</a>
+                </li>
+                <li><a href="https://api.skylerdong.com/" target="_blank" rel=”noreferrer”>New API back-end.</a>
+                </li>
               </ul>
             </li>
           </ul>
         </div>
         <div>
-          <p>I believe technology and art work hand in hand. I have longtime interests in design, calligraphy and <a href="<?php echo PATH; ?>/photography">photography</a>. Check out my <a href="<?php echo PATH; ?>/photography">photography gallery</a>.</p>
+          <p>I believe art and technology work hand in hand. Check out my <a href="<?php echo PATH; ?>/photography">photography gallery</a>.</p>
         </div>
       </div>
       <!--column-->
@@ -73,4 +79,5 @@ include_once '../common/htmlTag.php';
   <!--maincontent-->
   <?php include_once '../common/footer.php'; ?>
 </body>
+
 </html>
