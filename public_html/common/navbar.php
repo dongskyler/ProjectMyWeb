930025e3defff5b54c74bd1b451ca399c7025363
Skyler Dong <dongskyler@gmail.com>
2020-07-13 11:45:48 -0500

    Add resume to navbar (#55)

--- public_html/common/navbar.php
+++ public_html/common/navbar.php
@@ -1,22 +1,21 @@
 <!-- Navbar -->
 <style>
-.navbar {font-family: 'Ubuntu', sans-serif;}
+  .navbar {
+    font-family: 'Ubuntu', sans-serif;
+  }
 </style>
 <nav class="navbar sticky-top navbar-expand-sm
 <?php
-  if (!defined('NAVBAR_COLOR') || NAVBAR_COLOR=='lightdark') {
-    echo ' navbar-dark bg-lightdark';
-  }
-  elseif (NAVBAR_COLOR == 'dark') {
-    echo ' navbar-dark bg-deepdark';
-  }
-  elseif (NAVBAR_COLOR == 'light') {
-    echo ' navbar-light bg-light';
-  }
-  else echo ' navbar-dark bg-lightdark';
+if (!defined('NAVBAR_COLOR') || NAVBAR_COLOR == 'lightdark') {
+  echo ' navbar-dark bg-lightdark';
+} elseif (NAVBAR_COLOR == 'dark') {
+  echo ' navbar-dark bg-deepdark';
+} elseif (NAVBAR_COLOR == 'light') {
+  echo ' navbar-light bg-light';
+} else echo ' navbar-dark bg-lightdark';
 ?>
 ">
-  <a class="navbar-brand fade-in-fast" href="<?php echo PATH;?>">Skyler Dong</a>
+  <a class="navbar-brand fade-in-fast" href="<?php echo PATH; ?>">Skyler Dong</a>
   <button id="nav-icon" class="navbar-toggler fade-in-fast" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span></span>
     <span></span>
@@ -29,6 +28,9 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="<?php echo PATH; ?>/about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://api.skylerdong.com/resume/" target="_blank" rel="noreferrer noopener">Resume</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="<?php echo PATH; ?>/blog">Blog</a>
